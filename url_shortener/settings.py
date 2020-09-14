cfab4015cfb534ad1e51cea635e491ec0febb43a
Ayush Bajaj <bajaj.ayush2003.com>
2020-09-14 10:02:58 +0530

    form fixed

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$vjqib-&h*v3$9s!)o*jw^@cupy*9&zh@905(krhp!(o4f1&wv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["dikhe.ga","www.dikhe.ga"]
 
 
 # Application definition
